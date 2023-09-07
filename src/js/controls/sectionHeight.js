f847b3e3280786b610bb43fdd86ab43229872729
Nazar Marchenko <mp181.marchenko.nazar@gmail.com>
2023-09-07 16:09:31 +0300

    v2

--- src/js/controls/sectionHeight.js
+++ src/js/controls/sectionHeight.js
@@ -3,12 +3,18 @@
 function addClassSectionFullpage() {
 	const screenWidth = window.innerWidth;
 	const sections = document.querySelectorAll('main .section');
-
 	if (screenWidth >= 1024) {
-		sections.forEach(section => section.classList.add('fullpage'))
+		sections.forEach(section => {
+			if (section.id !== 'hero')
+				section.classList.add('fullpage')
+		})
 	} else {
-		sections.forEach(section => section.classList.remove('fullpage'))
 
+		sections.forEach(section => {
+			if (section.id !== 'hero') {
+				section.classList.remove('fullpage')
+			}
+		})
 	}
 }
 
