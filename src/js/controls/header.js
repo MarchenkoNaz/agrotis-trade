2feadbfa9f66d11c25f617dfc427bb81661a57b4
Nazar Marchenko <mp181.marchenko.nazar@gmail.com>
2023-11-02 09:01:18 +0200

    'add-elevator-page'

--- src/js/controls/header.js
+++ src/js/controls/header.js
@@ -1,37 +1,33 @@
-let lastScroll = 0;
 const defaultOffset = 200;
 const header = document.querySelector('.header');
 const headerMenu = document.querySelector('.navigation');
 const burgerButton = document.querySelector('.burger-menu-btn');
 
-const scrollPosition = () => window.scrollY || document.documentElement.scrollTop;
-const containHide = () => header.classList.contains('hide');
-
 const screenWidth = () => window.innerWidth || document.documentElement.clientWidth;
+let lastScroll = 0;
 
 const toggleHeader = () => {
-
-	if (scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
-
+	const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+	const isHeaderHidden = header.classList.contains('hide');
+	if (scrollPosition > lastScroll && !isHeaderHidden && scrollPosition > defaultOffset) {
 		header.classList.add('hide');
 		headerMenu.classList.remove('menu-open');
 		burgerButton.classList.remove('active');
-
-	}
-	else if (scrollPosition() < lastScroll && containHide()) {
+	} else if (scrollPosition < lastScroll && isHeaderHidden) {
 		header.classList.remove('hide');
-
 	}
-	lastScroll = scrollPosition();
+	lastScroll = scrollPosition;
 };
 
-window.addEventListener('scroll', () => {
+const handleScroll = () => {
 	if (screenWidth() <= 1024) {
 		toggleHeader();
 	}
-});
+};
 
+window.addEventListener('scroll', handleScroll);
 
+// Викликаємо функцію при завантаженні сторінки, щоб ініціалізувати стан заголовка
 if (screenWidth() <= 1024) {
 	toggleHeader();
 }
